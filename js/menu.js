3f1a2f6ec96644f2c1e3612cb49330c6bce6ad7a
nickozy <104327959+nickozy@users.noreply.github.com>
2022-06-29 16:32:19 +0300

    1

--- js/menu.js
+++ js/menu.js
@@ -3,19 +3,21 @@
 const navBtnOpen = document.getElementById("nav-burger"); // кнопка бюргер в меню
 const mainContentArea = document.getElementById("scrollview"); // главное окно с контентом
 const navMenuList = document.getElementById("navigation"); // выезжающая навигация по сайту
+const returnMainArea = document.getElementById("open-scroll");
 
 navBtnOpen.addEventListener("click", function () {
   mainContentArea.classList.add("scroll-view--menu-open");
   navMenuList.classList.add("menu--menu-open");
   document.querySelector("html").classList.remove("has-scroll-scrolling"); // удаляем класс со стилями скролла locomotive
   mainContentArea.style.overflow = "hidden";
+  returnMainArea.classList.add("open-scroll--menu-open");
   show();
 });
 
-navMenuList.addEventListener("click", function () {
+returnMainArea.addEventListener("click", function () {
   mainContentArea.classList.remove("scroll-view--menu-open");
-
   navMenuList.classList.remove("menu--menu-open");
+  returnMainArea.classList.remove("open-scroll--menu-open");
   setTimeout(showHide, 500);
 });
 
