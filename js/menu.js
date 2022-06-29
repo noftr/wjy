57f15affc2d94bc01de7670269ad4d1d0384a274
nickozy <104327959+nickozy@users.noreply.github.com>
2022-06-29 18:31:26 +0300

    1

--- js/menu.js
+++ js/menu.js
@@ -5,7 +5,7 @@ const mainContentArea = document.getElementById("scrollview"); // главное
 const navMenuList = document.getElementById("navigation"); // выезжающая навигация по сайту
 const returnMainArea = document.getElementById("open-scroll");
 const mainNav = document.getElementById("main-nav");
-
+const menuClose = document.getElementById("menu-close");
 navBtnOpen.addEventListener("click", function () {
   mainNav.classList.add("nav--menu-open");
   mainContentArea.classList.add("scroll-view--menu-open");
@@ -13,8 +13,8 @@ navBtnOpen.addEventListener("click", function () {
   document.querySelector("html").classList.remove("has-scroll-scrolling"); // удаляем класс со стилями скролла locomotive
   mainContentArea.style.overflow = "hidden";
   returnMainArea.classList.add("open-scroll--menu-open");
-  $(".section-tag").css("right", "0em");
-  $(".wrapper").css("max-width", "100%");
+  $(".section-tag").css("right", "0em"); // jquery меняю стиль всем элементам
+  $(".wrapper").css("max-width", "100%"); // jquery меняю стиль всем элементам
   show();
 });
 
@@ -23,12 +23,22 @@ returnMainArea.addEventListener("click", function () {
   mainContentArea.classList.remove("scroll-view--menu-open");
   navMenuList.classList.remove("menu--menu-open");
   returnMainArea.classList.remove("open-scroll--menu-open");
-  $(".section-tag").css("right", "6.94em");
-  $(".wrapper").css("max-width", "calc(100% - 6.94em)");
+  $(".section-tag").css("right", "6.94em"); // jquery меняю стиль всем элементам
+  $(".wrapper").css("max-width", "calc(100% - 6.94em)"); // jquery меняю стиль всем элементам
   setTimeout(showHide, 500);
 });
 
-// ДЕЛАЕМ ВЫСОТУ 100vh а второй обратно авто
+menuClose.addEventListener("click", function () {
+  mainNav.classList.remove("nav--menu-open");
+  mainContentArea.classList.remove("scroll-view--menu-open");
+  navMenuList.classList.remove("menu--menu-open");
+  returnMainArea.classList.remove("open-scroll--menu-open");
+  $(".section-tag").css("right", "6.94em"); // jquery меняю стиль всем элементам
+  $(".wrapper").css("max-width", "calc(100% - 6.94em)"); // jquery меняю стиль всем элементам
+  setTimeout(showHide, 500);
+});
+
+// ДЕЛАЕМ ВЫСОТУ 100Vh А ВТОРОЙ ОБРАТНО НА АВТО
 
 function show() {
   mainContentArea.style.height = "100vh";
