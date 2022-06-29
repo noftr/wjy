4af121a74e24723eb05552d4cd8ecdaddbed7d63
nickozy <104327959+nickozy@users.noreply.github.com>
2022-06-29 15:42:00 +0300

    1

---
+++ js/menu.js
@@ -0,0 +1,34 @@
+// ВЫЕЗЖАЮЩЕЕ МЕНЮ
+
+const navBtnOpen = document.getElementById('nav-burger') // кнопка бюргер в меню
+const mainContentArea = document.getElementById('scrollview') // главное окно с контентом
+const navMenuList = document.getElementById('navigation') // выезжающая навигация по сайту
+
+navBtnOpen.addEventListener('click', function () {
+  mainContentArea.classList.add('scroll-view--menu-open')
+  document.body.classList.add('body-red')
+  navMenuList.classList.add('menu--menu-open')
+  document.querySelector('html').classList.remove('has-scroll-scrolling') // удаляем класс со стилями скролла locomotive
+  mainContentArea.style.overflow = 'hidden'
+  show()
+})
+
+navMenuList.addEventListener('click', function () {
+  mainContentArea.classList.remove('scroll-view--menu-open')
+
+  document.body.classList.remove('body-red')
+  navMenuList.classList.remove('menu--menu-open')
+
+  setTimeout(showHide, 500)
+})
+
+// ДЕЛАЕМ ВЫСОТУ 100vh а второй обратно авто
+
+function show() {
+  mainContentArea.style.height = '100vh'
+}
+
+function showHide() {
+  mainContentArea.style.height = 'auto'
+  mainContentArea.style.overflow = 'visible'
+}
