ffa0ac1afb2361230c7a9581ed5f422dea09b088
noftr <97229373+noftr@users.noreply.github.com>
2022-07-07 12:11:52 +0300

    add actual 3d model

--- js/spline.js
+++ js/spline.js
@@ -1,23 +1,27 @@
-let obj;
+
+
+let sphereLight1, sphereLight2, sphereGroup, sphereLightGroup;
 
 import { Application } from "@splinetool/runtime";
 
-
 const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
-app.load("https://prod.spline.design/C8v5wKSdTmx0JFF1/scene.splinecode")
+app.load("https://prod.spline.design/p6OqcnDa7N85sCvb/scene.splinecode")
 .then(() => {
-    obj = app.findObjectById("908e4bcc-4410-49e2-85a6-230d2c4fcd79");
-    setTimeout(animationCube, 2000);
-    renderAnimation();
+    sphereLight1 = app.findObjectByName("sphereLight1");
+    sphereLight2 = app.findObjectByName("sphereLight2");
+    sphereGroup = app.findObjectByName("sphereGroup");
+    sphereLightGroup = app.findObjectByName("sphereLightGroup");
+    setTimeout(animationSphereIn, 4000);
+    setTimeout(animationSphereOut, 80000);
 });
 
-function animationCube() {
-    obj.emitEvent("mouseDown");
+function animationSphereIn() {
+    console.log("запустили 1 анимацию")
+    sphereLightGroup.emitEvent("keyDown");
 };
 
-function renderAnimation() {
-  console.log("начали цикл")
-	obj.rotation.x += .005;
-  window.requestAnimationFrame(renderAnimation);
+function animationSphereOut() {
+    console.log("запустили 2 анимацию")
+    sphereLightGroup.emitEvent("keyDown");
 };
