c967ad304d21f562e394e8ee27cc77079ed24cdc
noftr <97229373+noftr@users.noreply.github.com>
2022-07-07 15:51:05 +0300

    add 1 scene of spline 3d

--- js/spline.js
+++ js/spline.js
@@ -8,8 +8,8 @@ const canvas = document.getElementById("canvas3d");
 const app = new Application(canvas);
 app.load("https://prod.spline.design/p6OqcnDa7N85sCvb/scene.splinecode")
 .then(() => {
-    sphereLight1 = app.findObjectByName("sphereLight1");
-    sphereLight2 = app.findObjectByName("sphereLight2");
+    // sphereLight1 = app.findObjectByName("sphereLight1");
+    // sphereLight2 = app.findObjectByName("sphereLight2");
     sphereGroup = app.findObjectByName("sphereGroup");
     sphereLightGroup = app.findObjectByName("sphereLightGroup");
 });
@@ -28,11 +28,15 @@ ScrollTrigger.create({
 function animationSphereIn() {
     console.log("animationSphereIn")
     sphereLightGroup.emitEvent("keyDown");
+    sphereGroup.emitEventReverse("keyDown");
 };
 
 function animationSphereOut() {
     console.log("animationSphereOut")
     sphereLightGroup.emitEventReverse("keyDown");
+    sphereGroup.emitEvent("keyDown");
+    // sphereLight1.emitEvent("keyDown");
+    // sphereLight2.emitEvent("keyDown");
 };
 
 ScrollTrigger.create({
