f6aa6a5be3f4060701f95afe8efccfed6643fff6
noftr <97229373+noftr@users.noreply.github.com>
2022-07-07 13:09:46 +0300

    add gsap triggers on spline.js

--- js/spline.js
+++ js/spline.js
@@ -12,16 +12,66 @@ app.load("https://prod.spline.design/p6OqcnDa7N85sCvb/scene.splinecode")
     sphereLight2 = app.findObjectByName("sphereLight2");
     sphereGroup = app.findObjectByName("sphereGroup");
     sphereLightGroup = app.findObjectByName("sphereLightGroup");
-    setTimeout(animationSphereIn, 4000);
-    setTimeout(animationSphereOut, 80000);
+});
+
+ScrollTrigger.create({
+  trigger: ".trigger__slide-is1",
+  scroller: scrollerPage,
+  start: "top center",
+  end: "bottom center",
+  onEnter: animationSphereIn,
+  onLeave: animationSphereOut,
+  onEnterBack: animationSphereIn,
+  onLeaveBack: animationSphereOut,
+  markers: true,
 });
 
 function animationSphereIn() {
-    console.log("запустили 1 анимацию")
+    console.log("animationSphereIn")
     sphereLightGroup.emitEvent("keyDown");
 };
 
 function animationSphereOut() {
     console.log("запустили 2 анимацию")
-    sphereLightGroup.emitEvent("keyDown");
+    sphereLightGroup.emitEventReverse("keyDown");
+};
+
+ScrollTrigger.create({
+  trigger: ".trigger__slide-is2",
+  scroller: scrollerPage,
+  start: "top center",
+  end: "bottom center",
+  onEnter: animationSphereIn,
+  onLeave: animationSphereOut,
+  onEnterBack: animationSphereIn,
+  onLeaveBack: animationSphereOut,
+  markers: true,
+});
+
+function animationTriangleIn() {
+    console.log("запустили 1 анимацию")
+};
+
+function animationTriangleOut() {
+    console.log("запустили 2 анимацию")
+};
+
+ScrollTrigger.create({
+  trigger: ".trigger__slide-is3",
+  scroller: scrollerPage,
+  start: "top center",
+  end: "bottom center",
+  onEnter: animationSphereIn,
+  onLeave: animationSphereOut,
+  onEnterBack: animationSphereIn,
+  onLeaveBack: animationSphereOut,
+  markers: true,
+});
+
+function animationLayersIn() {
+    console.log("запустили 1 анимацию")
+};
+
+function animationLayersOut() {
+    console.log("запустили 2 анимацию")
 };
