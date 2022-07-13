3bddee65b62889ddec831fddd1812bb6b1a9e679
noftr <97229373+noftr@users.noreply.github.com>
2022-07-13 22:54:09 +0300

    add animation order

--- js/spline-2.js
+++ js/spline-2.js
@@ -60,16 +60,276 @@ app.load('https://prod.spline.design/2ofCLegPKjR2TqqZ/scene.splinecode')
 
 
 
+
+
+
+
+let whereTriggerActive = 0;
+let pastTrigger = 0;
 let playAnimation = false;
 
-let whoPlayAnimation;
+startAnimationScenes();
+function startAnimationScenes() {
 
-whoPlayAnimation = 1;
-whoPlayAnimation = 2;
-whoPlayAnimation = 3;
+		if (playAnimation == false){
+				switch (whereTriggerActive) {
+						case 0:
+								switch (pastTrigger) {
+										case 1:
+												playAnimation = true;
+												out1();
+												setTimeout(() => {
+														pastTrigger = 0;
+														playAnimation = false;
+												}, 3000);
+												break
 
-let whoPlayNext;
+										case 2:
+												playAnimation = true;
+												allScenes.emitEventReverse("keyDown");
+												out2();
+												setTimeout(() => {
+														pastTrigger = 0;
+														playAnimation = false;
+												}, 3000);
+												break
 
-whoPlayAnimation = 1;
-whoPlayAnimation = 2;
-whoPlayAnimation = 3;
+										case 3:
+												playAnimation = true;
+												allScenes.emitEventReverse("keyDown");
+												allScenesGroup.emitEventReverse("keyDown");
+												out3();
+												setTimeout(() => {
+														pastTrigger = 0;
+														playAnimation = false;
+												}, 3000);
+												break
+								};
+								break
+
+						case 1:
+								switch (pastTrigger) {
+										case 0:
+												playAnimation = true;
+												in1();
+												setTimeout(() => {
+														pastTrigger = 1;
+														playAnimation = false;
+												}, 3000);
+												break
+
+										case 2:
+												playAnimation = true;
+												out2();
+												setTimeout(() => {
+														allScenes.emitEventReverse("keyDown");
+														in1();
+														setTimeout(() => {
+																pastTrigger = 1;
+																playAnimation = false;
+														}, 3000);
+												}, 3000);
+												break
+
+										case 3:
+												playAnimation = true;
+												out3();
+												setTimeout(() => {
+														allScenes.emitEventReverse("keyDown");
+														allScenesGroup.emitEventReverse("keyDown");
+														in1();
+														setTimeout(() => {
+																pastTrigger = 1;
+																playAnimation = false;
+														}, 3000);
+												}, 3000);
+												break
+								};
+								break
+
+						case 2:
+								switch (pastTrigger) {
+										case 0:
+												playAnimation = true;
+												allScenes.emitEvent("keyDown");
+												in2();
+												setTimeout(() => {
+														pastTrigger = 2;
+														playAnimation = false;
+												}, 3000);
+												break
+
+										case 1:
+												playAnimation = true;
+												out1();
+												setTimeout(() => {
+														allScenes.emitEvent("keyDown");
+														in2();
+														setTimeout(() => {
+																pastTrigger = 2;
+																playAnimation = false;
+														}, 3000);
+												}, 3000);
+												break
+
+										case 3:
+												playAnimation = true;
+												out3();
+												setTimeout(() => {
+														allScenesGroup.emitEventReverse("keyDown");
+														in2();
+														setTimeout(() => {
+																pastTrigger = 2;
+																playAnimation = false;
+														}, 3000);
+												}, 3000);
+												break
+								};
+								break
+
+						case 3:
+								switch (pastTrigger) {
+										case 0:
+												playAnimation = true;
+												allScenes.emitEvent("keyDown");
+												allScenesGroup.emitEvent("keyDown");
+												in3();
+												setTimeout(() => {
+														pastTrigger = 3;
+														playAnimation = false;
+												}, 3000);
+												break
+
+										case 1:
+												playAnimation = true;
+												out1();
+												setTimeout(() => {
+														allScenes.emitEvent("keyDown");
+														allScenesGroup.emitEvent("keyDown");
+														in3();
+														setTimeout(() => {
+																pastTrigger = 3;
+																playAnimation = false;
+														}, 3000);
+												}, 3000);
+												break
+
+										case 2:
+												playAnimation = true;
+												out2();
+												setTimeout(() => {
+														allScenesGroup.emitEvent("keyDown");
+														in3();
+														setTimeout(() => {
+																pastTrigger = 3;
+																playAnimation = false;
+														}, 3000);
+												}, 3000);
+												break
+								};
+								break
+				}
+		};
+		requestAnimationFrame(startAnimationScenes);
+};
+
+
+function in1() {
+		sphereLightGroup.emitEvent("keyDown");
+		sphereGroup.emitEventReverse("keyDown");
+		console.log("in1")
+};
+
+function out1() {
+		sphereLightGroup.emitEventReverse("keyDown");
+		sphereGroup.emitEvent("keyDown");
+		console.log("out1")
+}
+
+function in2() {
+		setTimeout(() => {
+				pyramidPointLight.emitEvent("keyDown");
+		}, 1000);
+		pyramid1.emitEvent("keyDown");
+		pyramid2.emitEvent("keyDown");
+		pyramid3.emitEvent("keyDown");
+		mlp.emitEvent("keyDown");
+		desirable.emitEvent("keyDown");
+		viable.emitEvent("keyDown");
+		feasible.emitEvent("keyDown");
+		console.log("in2")
+};
+
+function out2() {
+		pyramidPointLight.emitEventReverse("keyDown");
+		pyramid1.emitEventReverse("keyDown");
+		pyramid2.emitEventReverse("keyDown");
+		pyramid3.emitEventReverse("keyDown");
+		mlp.emitEventReverse("keyDown");
+		desirable.emitEventReverse("keyDown");
+		viable.emitEventReverse("keyDown");
+		feasible.emitEventReverse("keyDown");
+		console.log("out2")
+}
+
+function in3() {
+		document.querySelector('.canvas-container').style.width = "2px"
+		console.log("in3")
+};
+
+function out3() {
+		console.log("out3")
+}
+
+
+
+
+
+
+
+// animation sphere
+ScrollTrigger.create({
+	  trigger: ".trigger__slide-is1",
+	  scroller: scrollerPage,
+	  start: "top center",
+	  end: "bottom center",
+	  onEnter: setTrigger1,
+	  onEnterBack: setTrigger1,
+	  onLeaveBack: setTrigger0,
+});
+
+ScrollTrigger.create({
+	  trigger: ".trigger__slide-is2",
+	  scroller: scrollerPage,
+	  start: "top center",
+	  end: "bottom center",
+	  onEnter: setTrigger2,
+	  onEnterBack: setTrigger2,
+});
+
+ScrollTrigger.create({
+	  trigger: ".trigger__slide-is3",
+	  scroller: scrollerPage,
+	  start: "top center",
+	  end: "bottom center",
+	  onEnter: setTrigger3,
+	  onEnterBack: setTrigger3,
+	  onLeave: setTrigger0,
+});
+
+
+function setTrigger0() {
+		whereTriggerActive = 0;
+};
+
+function setTrigger1() {
+		whereTriggerActive = 1;
+};
+
+function setTrigger2() {
+		whereTriggerActive = 2;
+};
+
+function setTrigger3() {
+		whereTriggerActive = 3;
+};
