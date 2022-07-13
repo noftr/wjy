444bd855b772dc72fd8e89f8eb776681a9a364ae
noftr <97229373+noftr@users.noreply.github.com>
2022-07-13 15:09:10 +0300

    clear trash code

--- js/spline.js
+++ js/spline.js
@@ -62,138 +62,50 @@ app.load('https://prod.spline.design/2ofCLegPKjR2TqqZ/scene.splinecode')
 
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-
-
-
-
-		// animation sphere
-		ScrollTrigger.create({
-		  trigger: ".trigger__slide-is1",
-		  scroller: scrollerPage,
-		  start: "top center",
-		  end: "bottom center",
-		  onEnter: sphereOnEnter,
-		  onEnterBack: sphereOnEnterBack,
-		  onLeaveBack: sphereLeaveBack,
-		  onLeave: sphereLeave,
-		});
-
-		function sphereOnEnter() {
-		    sphereLightGroup.emitEvent("keyDown");
-		    sphereGroup.emitEventReverse("keyDown");
-		    console.log("sphereOnEnter");
-		};
-
-		function sphereOnEnterBack() {
-				sphereGroup.emitEventReverse("keyDown");
-				setTimeout(() => {
-						sphereLightGroup.emitEvent("keyDown");
-				}, 1100);
-		    console.log("sphereOnEnterBack")
-				// clearTimeout(timerPyramidsOnEnterBack);
-				// clearTimeout(timerPyramidsOnEnter);
-		};
-
-		function sphereLeaveBack() {
-				sphereLightGroup.emitEventReverse("keyDown");
-				sphereGroup.emitEvent("keyDown");
-		    console.log("sphereLeaveBack")
-		};
-
-		function sphereLeave() {
-				sphereLightGroup.emitEventReverse("keyDown");
-				sphereGroup.emitEvent("keyDown");
-		    console.log("sphereLeave")
-		};
 
 
 
 
 
+// animation sphere
+ScrollTrigger.create({
+	  trigger: ".trigger__slide-is1",
+	  scroller: scrollerPage,
+	  start: "top center",
+	  end: "bottom center",
+	  onEnter: sphereOnEnter,
+	  onEnterBack: sphereOnEnterBack,
+	  onLeaveBack: sphereLeaveBack,
+	  onLeave: sphereLeave,
+});
 
+function sphereOnEnter() {
+    sphereLightGroup.emitEvent("keyDown");
+    sphereGroup.emitEventReverse("keyDown");
+    console.log("sphereOnEnter");
+};
 
+function sphereOnEnterBack() {
+		sphereGroup.emitEventReverse("keyDown");
+		setTimeout(() => {
+				sphereLightGroup.emitEvent("keyDown");
+		}, 1100);
+    console.log("sphereOnEnterBack")
+		// clearTimeout(timerPyramidsOnEnterBack);
+		// clearTimeout(timerPyramidsOnEnter);
+};
 
+function sphereLeaveBack() {
+		sphereLightGroup.emitEventReverse("keyDown");
+		sphereGroup.emitEvent("keyDown");
+    console.log("sphereLeaveBack")
+};
 
-		// animations pyramids
-		ScrollTrigger.create({
-		  trigger: ".trigger__slide-is2",
-		  scroller: scrollerPage,
-		  start: "top center",
-		  end: "bottom center",
-		  onEnter: pyramidsOnEnter,
-		  onEnterBack: pyramidsOnEnterBack,
-		  onLeaveBack: pyramidsLeaveBack,
-		  onLeave: pyramidsLeave,
-		});
-
-		function pyramidsOnEnter() {
-		    console.log("pyramidsOnEnter");
-
-				timerPyramidsOnEnter = setTimeout(() => {
-						// clearTimeout(timerPyramidsLeaveBack);
-				    allScenes.emitEvent("keyDown");
-
-						pyramidPointLight.emitEvent("keyDown");
-						pyramid1.emitEvent("keyDown");
-						pyramid2.emitEvent("keyDown");
-						pyramid3.emitEvent("keyDown");
-						mlp.emitEvent("keyDown");
-						desirable.emitEvent("keyDown");
-						viable.emitEvent("keyDown");
-						feasible.emitEvent("keyDown");
-				}, 1100);
-		};
-
-		function pyramidsOnEnterBack() {
-		    console.log("pyramidsOnEnterBack");
-
-				timerPyramidsOnEnterBack = setTimeout(() => {
-						pyramidPointLight.emitEvent("keyDown");
-						pyramid1.emitEvent("keyDown");
-						pyramid2.emitEvent("keyDown");
-						pyramid3.emitEvent("keyDown");
-						mlp.emitEvent("keyDown");
-						desirable.emitEvent("keyDown");
-						viable.emitEvent("keyDown");
-						feasible.emitEvent("keyDown");
-				}, 1100);
-		};
-
-		function pyramidsLeaveBack() {
-				// clearTimeout(timerPyramidsOnEnterBack);
-				// clearTimeout(timerPyramidsOnEnter);
-
-		    console.log("pyramidsLeaveBack")
-				timerPyramidsLeaveBack = setTimeout(() => {
-		    		allScenes.emitEventReverse("keyDown");
-				}, 1650);
-
-				pyramidPointLight.emitEventReverse("keyDown");
-				pyramid1.emitEventReverse("keyDown");
-				pyramid2.emitEventReverse("keyDown");
-				pyramid3.emitEventReverse("keyDown");
-				mlp.emitEventReverse("keyDown");
-				desirable.emitEventReverse("keyDown");
-				viable.emitEventReverse("keyDown");
-				feasible.emitEventReverse("keyDown");
-		};
-
-		function pyramidsLeave() {
-				// clearTimeout(timerPyramidsOnEnterBack);
-				// clearTimeout(timerPyramidsOnEnter);
-
-		    console.log("pyramidsLeave");
-
-				pyramidPointLight.emitEventReverse("keyDown");
-				pyramid1.emitEventReverse("keyDown");
-				pyramid2.emitEventReverse("keyDown");
-				pyramid3.emitEventReverse("keyDown");
-				mlp.emitEventReverse("keyDown");
-				desirable.emitEventReverse("keyDown");
-				viable.emitEventReverse("keyDown");
-				feasible.emitEventReverse("keyDown");
-		};
+function sphereLeave() {
+		sphereLightGroup.emitEventReverse("keyDown");
+		sphereGroup.emitEvent("keyDown");
+    console.log("sphereLeave")
+};
 
 
 
@@ -203,37 +115,123 @@ app.load('https://prod.spline.design/2ofCLegPKjR2TqqZ/scene.splinecode')
 
 
 
-		// animations plates
-		ScrollTrigger.create({
-		  trigger: ".trigger__slide-is3",
-		  scroller: scrollerPage,
-		  start: "top center",
-		  end: "bottom center",
-		  onEnter: platesOnEnter,
-		  onEnterBack: platesOnEnterBack,
-		  onLeaveBack: platesLeaveBack,
-		  onLeave: platesLeave,
-		});
+// animations pyramids
+ScrollTrigger.create({
+	  trigger: ".trigger__slide-is2",
+	  scroller: scrollerPage,
+	  start: "top center",
+	  end: "bottom center",
+	  onEnter: pyramidsOnEnter,
+	  onEnterBack: pyramidsOnEnterBack,
+	  onLeaveBack: pyramidsLeaveBack,
+	  onLeave: pyramidsLeave,
+});
 
-		function platesOnEnter() {
-				// clearTimeout(timerPlatesLeaveBack);
-		    allScenesGroup.emitEvent("keyDown");
-		    console.log("platesOnEnter");
-		};
+function pyramidsOnEnter() {
+    console.log("pyramidsOnEnter");
 
-		function platesOnEnterBack() {
-		    console.log("platesOnEnterBack")
-		};
+		timerPyramidsOnEnter = setTimeout(() => {
+				// clearTimeout(timerPyramidsLeaveBack);
+		    allScenes.emitEvent("keyDown");
 
-		function platesLeaveBack() {
-		    console.log("platesLeaveBack");
-				timerPlatesLeaveBack = setTimeout(() => {
-						allScenesGroup.emitEventReverse("keyDown");
-				}, 1650);
-		};
+				pyramidPointLight.emitEvent("keyDown");
+				pyramid1.emitEvent("keyDown");
+				pyramid2.emitEvent("keyDown");
+				pyramid3.emitEvent("keyDown");
+				mlp.emitEvent("keyDown");
+				desirable.emitEvent("keyDown");
+				viable.emitEvent("keyDown");
+				feasible.emitEvent("keyDown");
+		}, 1100);
+};
 
-		function platesLeave() {
-		    console.log("platesLeave");
-		};
+function pyramidsOnEnterBack() {
+    console.log("pyramidsOnEnterBack");
 
-// }, false);
+		timerPyramidsOnEnterBack = setTimeout(() => {
+				pyramidPointLight.emitEvent("keyDown");
+				pyramid1.emitEvent("keyDown");
+				pyramid2.emitEvent("keyDown");
+				pyramid3.emitEvent("keyDown");
+				mlp.emitEvent("keyDown");
+				desirable.emitEvent("keyDown");
+				viable.emitEvent("keyDown");
+				feasible.emitEvent("keyDown");
+		}, 1100);
+};
+
+function pyramidsLeaveBack() {
+		// clearTimeout(timerPyramidsOnEnterBack);
+		// clearTimeout(timerPyramidsOnEnter);
+
+    console.log("pyramidsLeaveBack")
+		timerPyramidsLeaveBack = setTimeout(() => {
+    		allScenes.emitEventReverse("keyDown");
+		}, 1650);
+
+		pyramidPointLight.emitEventReverse("keyDown");
+		pyramid1.emitEventReverse("keyDown");
+		pyramid2.emitEventReverse("keyDown");
+		pyramid3.emitEventReverse("keyDown");
+		mlp.emitEventReverse("keyDown");
+		desirable.emitEventReverse("keyDown");
+		viable.emitEventReverse("keyDown");
+		feasible.emitEventReverse("keyDown");
+};
+
+function pyramidsLeave() {
+		// clearTimeout(timerPyramidsOnEnterBack);
+		// clearTimeout(timerPyramidsOnEnter);
+
+    console.log("pyramidsLeave");
+
+		pyramidPointLight.emitEventReverse("keyDown");
+		pyramid1.emitEventReverse("keyDown");
+		pyramid2.emitEventReverse("keyDown");
+		pyramid3.emitEventReverse("keyDown");
+		mlp.emitEventReverse("keyDown");
+		desirable.emitEventReverse("keyDown");
+		viable.emitEventReverse("keyDown");
+		feasible.emitEventReverse("keyDown");
+};
+
+
+
+
+
+
+
+
+
+// animations plates
+ScrollTrigger.create({
+	  trigger: ".trigger__slide-is3",
+	  scroller: scrollerPage,
+	  start: "top center",
+	  end: "bottom center",
+	  onEnter: platesOnEnter,
+	  onEnterBack: platesOnEnterBack,
+	  onLeaveBack: platesLeaveBack,
+	  onLeave: platesLeave,
+});
+
+function platesOnEnter() {
+		// clearTimeout(timerPlatesLeaveBack);
+    allScenesGroup.emitEvent("keyDown");
+    console.log("platesOnEnter");
+};
+
+function platesOnEnterBack() {
+    console.log("platesOnEnterBack")
+};
+
+function platesLeaveBack() {
+    console.log("platesLeaveBack");
+		timerPlatesLeaveBack = setTimeout(() => {
+				allScenesGroup.emitEventReverse("keyDown");
+		}, 1650);
+};
+
+function platesLeave() {
+    console.log("platesLeave");
+};
